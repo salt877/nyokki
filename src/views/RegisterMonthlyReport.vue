ecb4bfa1f184937fbb98d984f3078a233b7a7c8c
shiodarina <rina.shioda@rakus-partners.co.jp>
2020-10-27 15:27:18 +0900

    月報登録画面を作成しました。

--- src/views/RegisterMonthlyReport.vue
+++ src/views/RegisterMonthlyReport.vue
@@ -1,11 +1,114 @@
 <template>
-  <div style="text-align: center">
-  <h2>月報登録</h2>
-</div>
+    <v-container>
+        <h2>月報登録</h2>
+         <v-row>
+            <v-col>
+                <v-card>
+                    <v-card-title>今月の目標</v-card-title>
+                    <v-card-text
+                        v-for="tmGoal in thisMonthsGoals"
+                        :key="tmGoal"
+                    >
+                        {{ tmGoal.text }}
+                    </v-card-text>
+                </v-card>
+            </v-col>
+         </v-row>
+         <v-row>
+            <v-col>
+                <v-card>
+                    <v-card-title>所感</v-card-title>
+                    <v-textarea
+                        class="mt-0"
+                        auto-grow
+                        rows="3"
+                        value=""
+                        placeholder="今月の振り返りをしよう！"
+                    >
+                    </v-textarea>
+                    <v-card-actions>
+                        <v-btn
+                            color="warning"
+                            @click="copyImpressions()"
+                            >コピー
+                        </v-btn>
+                    </v-card-actions>
+                </v-card> 
+            </v-col>
+         </v-row>  
+        <v-row>
+            <v-col>
+                <v-card>
+                    <v-card-title>来月の目標</v-card-title>
+                    <v-card-text
+                        v-for=" nmGoal in nextMonthsGoals"
+                        :key="nmGoal">
+                        {{ nmGoal.text }}
+                    </v-card-text>
+                    <v-text-field 
+                        placeholder="来月の目標は？"
+                        v-model="inputNextMonthsGoals"
+                        >
+                    </v-text-field> 
+                    <v-card-actions>
+                        <v-btn
+                            color="primary"
+                            @click="addNextMonthsTarget()"
+                        >
+                        追加
+                        </v-btn>
+                    </v-card-actions>
+                </v-card>  
+            </v-col>
+        </v-row>
+        <v-btn
+            color="error"
+            @click="saveMonthlyReport"
+            >保存する
+        </v-btn>
+    </v-container>
 </template>
 
 <script>
   export default {
-    name: "RegisterMonthlyReport"
+    name: "RegisterMonthlyReport",
+    data() {
+        return {
+            thisMonthsGoals:[
+                { text: '目標1'},
+                { text: '目標2'}
+            ],
+            inputNextMonthsGoals: '',
+            nextMonthsGoals: [
+                { text: '来月の目標1' },
+                { text: '来月の目標2' }
+            ]
+        }
+    },
+    methods: {
+        addNextMonthsTarget: function(){
+            const newMonthsGoals = this.inputNextMonthsGoals;
+            if (!newMonthsGoals){
+                return ;
+            }
+            this.nextMonthsGoals.push({
+                text: newMonthsGoals
+            });
+            this.inputNextMonthsGoals = '';
+        }
+    }
   };
 </script>
+
+<style scoped>
+.container {
+    text-align: center;
+}
+.v-input {
+    width: 90%;
+    margin: auto;
+}
+.v-btn {
+    margin: 0 0 0 auto;
+}
+</style>
