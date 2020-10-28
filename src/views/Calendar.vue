63b63715fa81c57a3b77e935154d7610d564896e
fuka0726 <fuuka.pyop@gmail.com>
2020-10-28 13:23:31 +0900

    折れ線グラフ作成,コンポーネント整理

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,17 +1,38 @@
 <template>
-  <div style="text-align: center">
-  <h2>カレンダー</h2>
-  <calendarComponent></calendarComponent>
-</div>
+<v-container>
+
+  
+  <v-row>
+    <v-col>
+        <CalendarComponent></CalendarComponent>
+    </v-col>
+    <v-col>
+      <v-card>
+        <Chart></Chart>
+      </v-card>
+    </v-col>
+  </v-row>
+  <v-row>
+    <v-col>
+      <p>日報・月報表示</p>
+    </v-col>
+    <v-col>
+      
+    </v-col>
+  </v-row>
+
+</v-container>
 </template>
 
 <script>
-import CalendarComponent from './CalendarComponent.vue';
+import CalendarComponent from '../components/CalendarComponent.vue';
+import Chart from '../components/Chart.vue';
 
   export default {
     name: "Calendar",
     components:{
-      CalendarComponent
+      CalendarComponent,
+      Chart
     },
     // data(){
     //   return{
