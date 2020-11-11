53d21c2b048300e9aff5154377b13b76be10b0bd
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jp  git config --list git config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2020-11-11 16:01:47 +0900

    通信の成功

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,64 +1,72 @@
-<template >
-    <div class="back-ground">
+<template>
+  <div class="back-ground">
     <div class="box">
-        <h3>毎日書いてにょきにょき育てる</h3>
-        <v-img src="../images/logo.jpg"></v-img>
-        <v-btn class="loginButton" color="green" @click="signIn"><v-icon>mdi-flower</v-icon>サインイン</v-btn>
-        <v-btn class="loginButton" color="green" @click="signIn2"><v-icon>mdi-flower</v-icon>サインイン2</v-btn>
+      <h3>毎日書いてにょきにょき育てる</h3>
+      <h3>{{ aaa }}{{ id }}{{ name }}</h3>
+
+      <v-img src="../images/logo.jpg"></v-img>
+      <v-btn class="loginButton" color="green" @click="signIn"><v-icon>mdi-flower</v-icon>サインイン</v-btn>
+      <v-btn class="loginButton" color="green" @click="signIn2"><v-icon>mdi-flower</v-icon>サインイン2</v-btn>
     </div>
-    </div>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
-    name: 'SignIn',
-    methods: {
-        signIn(){
-            axios.post("/user/signIn")
-            .then(( res ) => {
-                console.log(res.data);
-            alert('サインイン！');
-            })
-        },
-        signIn2(){
-            axios.post("/signIn")
-             Promise.resolve()
-            .then(( res ) => {
-                console.log(res);
-            alert('サインイン2！');
-            })
-        }            
-
-    }
+  data() {
+    return {
+      aaa: "",
+      id: "",
+      name: "",
+    };
+  },
+  name: "SignIn",
+  methods: {
+    signIn() {
+      axios.post("/user/signIn").then((res) => {
+        console.log(res.data);
+        this.aaa = res.data;
+        alert("サインイン！");
+      });
+    },
+    signIn2() {
+      axios.post("/users").then((res) => {
+        console.log(res.data);
+        this.name = res.data.name;
+        this.id = res.data.id;
+        alert("サインイン2！");
+      });
+    },
+  },
 };
 </script>
 
 <style>
 .back-ground {
-    background-image: url("~@/assets/SignInBackGround.jpg");
-    background-size: cover;
-    background-position: center center;
-    width: 100%;
-    height: 100vh;
-    position: relative;
+  background-image: url("~@/assets/SignInBackGround.jpg");
+  background-size: cover;
+  background-position: center center;
+  width: 100%;
+  height: 100vh;
+  position: relative;
 }
 .box {
-    text-align: center;
-    position: relative;
-    top: 20vh;
-    width: 70%;
-    height: 50%;
-    background-color:rgba(255, 255, 255, 0.671);
-    margin: 0 auto;
+  text-align: center;
+  position: relative;
+  top: 20vh;
+  width: 70%;
+  height: 50%;
+  background-color: rgba(255, 255, 255, 0.671);
+  margin: 0 auto;
 }
 .box h1 {
-    margin: 5vh 0;
+  margin: 5vh 0;
 }
 .v-image {
-    width: 300px;
-    height: auto;
-    margin: 3vh auto;
+  width: 300px;
+  height: auto;
+  margin: 3vh auto;
 }
 </style>
