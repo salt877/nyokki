be4cb39f25a138c5f27efd6b86f801ccea3e33e2
fuka0726 <fuuka.pyop@gmail.com>
2020-12-11 17:28:36 +0900

    達成度グラフにデータを反映させました(期間指定のしばりあり、カレンダーとは連動してない)

---
+++ src/components/LineChart.vue
@@ -0,0 +1,23 @@
+<script>
+  import { Line } from 'vue-chartjs'
+
+
+  export default {
+    extends: Line,
+    name: 'LineChart',
+    props: {
+    data: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null
+    }
+  },
+    
+    mounted () {
+      this.renderChart(this.data, this.options)
+    }
+  }
+</script>
