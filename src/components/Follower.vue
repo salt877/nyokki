4b079c94502506c469a81b7b94ce956f81e85166
yusukeAshibe <yusuke.ashibe@rakus-partners.co.jp  git config --list git config --global user.email yusuke.ashibe@rakus-partners.co.jp>
2021-01-29 10:55:11 +0900

    途中保存

--- src/components/Follower.vue
+++ src/components/Follower.vue
@@ -1,128 +1,112 @@
 <template>
-<v-container>
-  <h2 style="text-align: center">{{ this.$store.state.loginUser.name }}さんのフォロワー</h2>
-  <v-row>
-    <v-col cols="12" sm="6" offset-sm="3">
-      <v-card>
-        <v-list two-line>
-          <template v-for="(item, index) in followerUserList.slice(0, 6)">
-            <v-subheader v-if="item.header" :key="item.header">
-              {{ item.header }}
-            </v-subheader>
+  <v-container>
+    <h2 style="text-align: center">{{ this.$store.state.loginUser.name }}さんのフォロワー</h2>
+    <v-row>
+      <v-col cols="12" sm="6" offset-sm="3">
+        <v-card>
+          <v-list two-line>
+            <template v-for="(item, index) in followerUserList.slice(0, 6)">
+              <v-subheader v-if="item.header" :key="item.header">
+                {{ item.header }}
+              </v-subheader>
 
-            <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
+              <v-divider v-else-if="item.divider" :key="index" :inset="item.inset"></v-divider>
 
-            <v-list-item v-else :key="item.title">
-              <v-list-item-avatar size="90">
-                <img :src="item.photoUrl">
-              </v-list-item-avatar>
+              <v-list-item v-else :key="item.title">
+                <v-list-item-avatar size="90">
+                  <img :src="item.photoUrl" />
+                </v-list-item-avatar>
 
-              <v-list-item-content>
-                <v-list-item-title v-html="item.userName"></v-list-item-title>
-              </v-list-item-content>
+                <v-list-item-content>
+                  <v-list-item-title v-html="item.userName"></v-list-item-title>
+                </v-list-item-content>
 
-              <v-list-item-action> 
-                <v-list-item-action-text></v-list-item-action-text>
+                <v-list-item-action>
+                  <v-list-item-action-text></v-list-item-action-text>
                   <v-card-actions>
-                    <v-btn color="amber darken-1" v-if="item.followFlag===true" v-model="followFlag" disabled>フォロー許可済</v-btn>
+                    <v-btn color="amber darken-1" v-if="item.followFlag === true" v-model="followFlag" disabled>フォロー許可済</v-btn>
                   </v-card-actions>
-                  <v-card-actions v-if="item.followFlag===false" v-model="followFlag">
-                    <v-btn color="light-blue lighten-3"  @click="approve(item)">承認⭕️</v-btn>
+                  <v-card-actions v-if="item.followFlag === false" v-model="followFlag">
+                    <v-btn color="light-blue lighten-3" @click="approve(item)">承認⭕️</v-btn>
                     <v-btn color="pink lighten-4" @click="deny(item)">否認❌</v-btn>
                   </v-card-actions>
-              </v-list-item-action>
-            </v-list-item>
-          </template>
-        </v-list>
-      </v-card>
-    </v-col>
-  </v-row>
-
-</v-container>
+                </v-list-item-action>
+              </v-list-item>
+            </template>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import axios from 'axios';
-  export default {
-    data: () => ({
-      // items: [
-      //   { header: '成長を分かちあおう🌱' },
-      //   { 
-      //     title: 'ユーザーG',
-      //      subtitle: '咲かせた花数🌷：10🌸'},
-      //   { divider: true, inset: true },
-      //   { 
-      //   title: 'ユーザーH', subtitle: '咲かせた花数🌷：50🌸'},
-      //   { divider: true, inset: true },
-      //   { title: 'ユーザーI', 
-      //   subtitle: '咲かせた花数🌷：100🌸',
-      //    },
-      // ],
-      item: [{
-        value: 'userId',
-      }],
-      followFlag: true,
-      followerUserList:[],
-    }),
-    props: ["followerList"],
-    created() {
-    
-      let followerUserList = [];
-
-        this.followerList.forEach(user => {
-          const createUserList = {
-            userId: user.id,
-            userName: user.name,
-            photoUrl: user.photoUrl,
-            followingsId: user.followingsId,
-            followFlag: user.followFlag,
-            followingId: user.followingId,
-            followedId: user.followedId
-          };
-            followerUserList.push(createUserList); 
-
-        })
-        this.followerUserList = followerUserList;
-    },
-    methods: {
-      //フォローを承認する
-      approve(item){
-        axios.post("/get/approveFollowRequest",{loginUser: this.$store.state.loginUser, followingsId: item.followingsId,followFlag: item.followFlag,
-          followingId: item.followingId,followedId: item.followedId });
-          alert("フォローを許可を承認しました。");
-          item.followFlag = true;
-          console.log(this.followerUserList);
-
-          let followedLength = [];
-
-          this.followerUserList.forEach(follower => {
-            if(follower.followFlag === false){
-              return;
-              } else if(follower.followFlag === true){
-              followedLength.push(follower);
-            }
-          })
-              this.$emit("followedLength", followedLength.length);
+import axios from "axios";
+export default {
+  data: () => ({
+    item: [
+      {
+        value: "userId",
       },
-      deny(item){
-        axios.post("/get/denyFollowRequest",{loginUser: this.$store.state.loginUser, followingsId: item.followingsId,followFlag: item.followFlag,
-          followingId: item.followingId,followedId: item.followedId });
-          let check = confirm(item.userName+"さんのフォローを否認します。");
+    ],
+    followFlag: true,
+    followerUserList: [],
+  }),
+  props: ["followerList"],
+  created() {
+    let followerUserList = [];
 
-          if(check===true){
-            this.followerUserList.forEach(follower => {
-                
-                if(item.followingsId === follower.followingsId){
-                  let friendIndex = this.followerUserList.indexOf(follower);
-                  this.followerUserList.splice(friendIndex,1);
-                }
-              })
-                  this.$emit("followedLength", this.followerUserList.length);
-          } else {
-            return false;
+    this.followerList.forEach((user) => {
+      const createUserList = {
+        userId: user.id,
+        userName: user.name,
+        photoUrl: user.photoUrl,
+        followingsId: user.followingsId,
+        followFlag: user.followFlag,
+        followingId: user.followingId,
+        followedId: user.followedId,
+      };
+      followerUserList.push(createUserList);
+    });
+    this.followerUserList = followerUserList;
+  },
+  methods: {
+    //フォローを承認する
+    approve(item) {
+      axios.post("/get/approveFollowRequest", { loginUser: this.$store.state.loginUser, followingsId: item.followingsId, followFlag: item.followFlag, followingId: item.followingId, followedId: item.followedId }).then((res) => {
+        console.log(res.data);
+      });
+      alert("フォローを許可を承認しました。");
+      item.followFlag = true;
+      console.log(this.followerUserList);
+
+      let followedLength = [];
+
+      this.followerUserList.forEach((follower) => {
+        if (follower.followFlag === false) {
+          return;
+        } else if (follower.followFlag === true) {
+          followedLength.push(follower);
+        }
+      });
+      this.$emit("followedLength", followedLength.length);
+    },
+    deny(item) {
+      axios.post("/get/denyFollowRequest", { loginUser: this.$store.state.loginUser, followingsId: item.followingsId, followFlag: item.followFlag, followingId: item.followingId, followedId: item.followedId });
+      let check = confirm(item.userName + "さんのフォローを否認します。");
+
+      if (check === true) {
+        this.followerUserList.forEach((follower) => {
+          if (item.followingsId === follower.followingsId) {
+            let friendIndex = this.followerUserList.indexOf(follower);
+            this.followerUserList.splice(friendIndex, 1);
           }
-
+        });
+        this.$emit("followedLength", this.followerUserList.length);
+      } else {
+        return false;
       }
-    }
-  }
+    },
+  },
+};
 </script>
--- src/views/Mypage.vue
+++ src/views/Mypage.vue
@@ -1,153 +1,143 @@
 <template>
-  <v-main  class="back">
+  <v-main class="back">
     <!-- ナビゲーション -->
-      <navigation></navigation>
-  <v-container align-content="center">
-    <v-row>
-      <v-col class="user-name">
-          <v-avatar size="150" >
+    <navigation></navigation>
+    <v-container align-content="center">
+      <v-row>
+        <v-col class="user-name">
+          <v-avatar size="150">
             <v-img :src="photoUrl" />
-             <!-- <v-img :src="`${photoUrl}`" /> -->
-          </v-avatar><br><br>
-          <v-text :value2="newName" @input="newName = $event" justify="center">
-              ユーザー名：{{ this.$store.state.loginUser.name }}
-          </v-text>
-      </v-col>
-      <v-col class="user-flower">
-        <p>現在の花の状態：</p>
-        <v-list-item-avatar size="150">
-          <NyokkiFlower :flowerStatus="flowerStatus"></NyokkiFlower>
-        </v-list-item-avatar>
-      </v-col>
-    </v-row>
+            <!-- <v-img :src="`${photoUrl}`" /> --> </v-avatar
+          ><br /><br />
+          <v-text :value2="newName" @input="newName = $event" justify="center"> ユーザー名：{{ this.$store.state.loginUser.name }} </v-text>
+        </v-col>
+        <v-col class="user-flower">
+          <p>現在の花の状態：</p>
+          <v-list-item-avatar size="150">
+            <NyokkiFlower :flowerStatus="flowerStatus"></NyokkiFlower>
+          </v-list-item-avatar>
+        </v-col>
+      </v-row>
 
-    <v-row>
-      <v-col>
-          <v-card-actions >
-            <v-btn color="warning" @click="componentName='Follow'">フォロー一覧</v-btn>
-        <v-card-actions> 
-        <v-text>フォロー数：{{ followingLength }}人</v-text>
-        </v-card-actions>
-        </v-card-actions>      
-      </v-col>
-      <v-col>
-        <v-card-actions>
-            <v-btn color="warning" @click="componentName='Follower'" >フォロワー一覧</v-btn>
-        <v-card-actions>
-          <v-text>フォロワー数：{{ followedLength }}人</v-text>
-        </v-card-actions>
-        </v-card-actions>
-      </v-col>
-      <v-col>
-        <v-card-actions>
-            <v-btn color="warning" @click="componentName='ProfileChange'">プロフィール変更</v-btn>
-        </v-card-actions>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col>
-        <v-card>
-           <component :is="componentName" :followList="followList" :followerList="followerList" 
-            @followedLength="followedLength=$event" @followingLength="followingLength=$event"></component>
-        </v-card>
-      </v-col>
-    </v-row>
-    <v-row><v-col></v-col></v-row>
-    <v-row><v-col></v-col></v-row>
-  </v-container>
+      <v-row>
+        <v-col>
+          <v-card-actions>
+            <v-btn color="warning" @click="componentName = 'Follow'">フォロー一覧</v-btn>
+            <v-card-actions>
+              <v-text>フォロー数：{{ followingLength }}人</v-text>
+            </v-card-actions>
+          </v-card-actions>
+        </v-col>
+        <v-col>
+          <v-card-actions>
+            <v-btn color="warning" @click="componentName = 'Follower'">フォロワー一覧</v-btn>
+            <v-card-actions>
+              <v-text>フォロワー数：{{ followedLength }}人</v-text>
+            </v-card-actions>
+          </v-card-actions>
+        </v-col>
+        <v-col>
+          <v-card-actions>
+            <v-btn color="warning" @click="componentName = 'ProfileChange'">プロフィール変更</v-btn>
+          </v-card-actions>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-card>
+            <component :is="componentName" :followList="followList" :followerList="followerList" @followedLength="followedLength = $event" @followingLength="followingLength = $event"></component>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row><v-col></v-col></v-row>
+      <v-row><v-col></v-col></v-row>
+    </v-container>
   </v-main>
 </template>
 
 <script>
-import Navigation from '../components/Navigation';
-import axios from 'axios';
-import ProfileChange from '../components/ProfileChange.vue';
-import Follow from '../components/Follow.vue';
-import Follower from '../components/Follower.vue';
-import NyokkiFlower from '../components/NyokkiFlower.vue';
+import Navigation from "../components/Navigation";
+import axios from "axios";
+import ProfileChange from "../components/ProfileChange.vue";
+import Follow from "../components/Follow.vue";
+import Follower from "../components/Follower.vue";
+import NyokkiFlower from "../components/NyokkiFlower.vue";
 //import {mapGetters} from 'vuex';
 //import {mapMutations} from 'vuex';
 //import {mapActions} from 'vuex';
 
-  export default {
-    name: "Mypage",
-    data: () => ({
-      newName: "",
-      componentName: ['Follow', 'Follower', 'ProfileChange'],
-      followingLength: [],
-      followedLength: [],
-      flowerStatus: "",
+export default {
+  name: "Mypage",
+  data: () => ({
+    newName: "",
+    componentName: ["Follow", "Follower", "ProfileChange"],
+    followingLength: [],
+    followedLength: [],
+    flowerStatus: "",
   }),
-    components:{
-      ProfileChange,
-      Follow,
-      Follower,
-      NyokkiFlower,
-      Navigation,
-    },
-    created(){
-    
-       axios.post("/get/followAndFollowerList",{ loginUser: this.$store.state.loginUser }).then(res=> {
-       
-        let flowerCount = this.$store.state.loginUser.continuationDays / 32;
-        let flowerStatus = this.$store.state.loginUser.continuationDays % 32;
+  components: {
+    ProfileChange,
+    Follow,
+    Follower,
+    NyokkiFlower,
+    Navigation,
+  },
+  created() {
+    axios.post("/get/followAndFollowerList", { loginUser: this.$store.state.loginUser }).then((res) => {
+      let flowerCount = this.$store.state.loginUser.continuationDays / 32;
+      let flowerStatus = this.$store.state.loginUser.continuationDays % 32;
 
-          if(flowerCount < 1){
-            flowerCount = 0;
-            this.flowerStatus = flowerStatus;
-
-          } else if(flowerCount >= 1){
-            flowerCount = Math.floor(flowerCount);
-            this.flowerStatus = flowerStatus;
-          }
-
-        const followList = res.data.followList;
-        const followerList = res.data.followerList;
-        this.followList = followList;
-        this.followerList = followerList;
-        
-        //フォローしている人数
-        const followingLength = [];
-        this.followList.forEach(follow => {
-          followingLength.push(follow);
-        })
-        this.followingLength = followingLength.length;
-        //フォロワー人数
-        const followerLength = [];
-        this.followerList.forEach(follower => {
-          if(follower.followFlag === true){
-            followerLength.push(follower);
-          } 
-        })
-        
-        this.followedLength = followerLength.length;
-
-       })
-    },
-    computed:{
-      //    ...mapGetters(["followingLength", "followedLength"]),
-      photoUrl(){
-        return this.$store.state.loginUser.photoUrl;
+      if (flowerCount < 1) {
+        flowerCount = 0;
+        this.flowerStatus = flowerStatus;
+      } else if (flowerCount >= 1) {
+        flowerCount = Math.floor(flowerCount);
+        this.flowerStatus = flowerStatus;
       }
-  }     
+
+      const followList = res.data.followList;
+      const followerList = res.data.followerList;
+      this.followList = followList;
+      this.followerList = followerList;
+
+      //フォローしている人数
+      const followingLength = [];
+      this.followList.forEach((follow) => {
+        followingLength.push(follow);
+      });
+      this.followingLength = followingLength.length;
+      //フォロワー人数
+      const followerLength = [];
+      this.followerList.forEach((follower) => {
+        if (follower.followFlag === true) {
+          followerLength.push(follower);
+        }
+      });
+
+      this.followedLength = followerLength.length;
+    });
+  },
+  computed: {
+    //    ...mapGetters(["followingLength", "followedLength"]),
+    photoUrl() {
+      return this.$store.state.loginUser.photoUrl;
+    },
+  },
 };
 </script>
 <style scoped>
 .container {
-    margin-top: 10px;
-    
+  margin-top: 10px;
 }
-.user-name{
+.user-name {
   text-align: center;
 }
 
-.back{
+.back {
   background-image: url("~@/assets/Background5.png");
   background-size: cover;
   background-position: center center;
   width: 100%;
   height: 100%;
 }
-
-
 </style>
